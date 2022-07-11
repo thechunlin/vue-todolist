50c9ff6f75c9e75ad892e07a875461dedbccebbe
Ying-Chun Lin <thechunlin@gmail.com>
2022-07-11 23:33:54 +0800

    build: 安裝 `vuex-persist`

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,9 @@
 import { createStore } from 'vuex'
+import VuexPersistence from 'vuex-persist'
+
+const vuexLocal = new VuexPersistence({
+  storage: window.localStorage
+})
 
 export default createStore({
   state: {
@@ -25,5 +30,6 @@ export default createStore({
     }
   },
   actions: {},
-  modules: {}
+  modules: {},
+  plugins: [vuexLocal.plugin]
 })
