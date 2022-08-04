c01e44f8eaf0b893bc6f880232096b08dc8193b9
Ying-Chun Lin <thechunlin@gmail.com>
2022-08-04 09:30:48 +0800

    更改背景色

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,8 @@
 
 <style lang="scss">
 #app {
+  background-color: rgb(205, 241, 241);
+  height: 100vh;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
